90d35cef182aa49cf24c7f18725bb5e7e6b30919
Bogdan Iusco <bogdan.iusco@gmail.com>
2015-01-09 15:01:11 +0200

    Add Cocoapods.

---
+++ Example/Pods/Target Support Files/Pods-BIObjCHelpersExampleTests-CJAMacros/Pods-BIObjCHelpersExampleTests-CJAMacros-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BIObjCHelpersExampleTests_CJAMacros : NSObject
+@end
+@implementation PodsDummy_Pods_BIObjCHelpersExampleTests_CJAMacros
+@end
---
+++ Example/Pods/Target Support Files/Pods-BIObjCHelpersExampleTests/Pods-BIObjCHelpersExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BIObjCHelpersExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_BIObjCHelpersExampleTests
+@end
