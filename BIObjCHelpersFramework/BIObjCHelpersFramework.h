b9bf623e2d51a548b503c0370d571288dd3f200b
Bogdan Iusco <bogdan.iusco@gmail.com>
2016-04-27 16:22:38 +0300

    Add framework target.

---
+++ BIObjCHelpersFramework/BIObjCHelpersFramework.h
@@ -0,0 +1,19 @@
+//
+//  BIObjCHelpersFramework.h
+//  BIObjCHelpersFramework
+//
+//  Created by Bogdan Iusco on 4/27/16.
+//  Copyright © 2016 iGama Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BIObjCHelpersFramework.
+FOUNDATION_EXPORT double BIObjCHelpersFrameworkVersionNumber;
+
+//! Project version string for BIObjCHelpersFramework.
+FOUNDATION_EXPORT const unsigned char BIObjCHelpersFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BIObjCHelpersFramework/PublicHeader.h>
+
+
